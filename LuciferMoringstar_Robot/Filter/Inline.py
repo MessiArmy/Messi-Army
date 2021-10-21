e21b7004635d47d34b93652b36756d027667d32d
MessiArmy <92917165+MessiArmy@users.noreply.github.com>
2021-10-21 14:25:42 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,10 +89,10 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('Support Group', url='https://t.me/SKMMovieWorld'),
+        InlineKeyboardButton('More Botz', url='https://t.me/SKMMovieWorld')
         ],[
-        InlineKeyboardButton('🖥️ Deploy Video 🖥️', url=f'{TUTORIAL}')
+        InlineKeyboardButton('🖥️ Deploy Video 🖥️', url='https://t.me/SKMMovieWorld')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
